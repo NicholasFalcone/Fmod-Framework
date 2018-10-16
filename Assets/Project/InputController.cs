fcd3cc27cf6d2f2a2d627e0aa3a1b049e1be0dc7
Nicholas Falcone <nicholasfal@live.it>
2018-10-16 21:56:57 +0200

    Added Light System

---
+++ Assets/Project/InputController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    private MovementComponent m_movement;
+
+    private Weapon m_weapon;
+
+    private void Awake()
+    {
+        m_movement = GetComponent<MovementComponent>();
+        m_weapon = GetComponent<Weapon>();
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        m_movement.Movement(h);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            m_movement.Jump();
+
+
+        if (Input.GetKeyDown(KeyCode.C) && m_weapon != null)
+            m_weapon.Fire();
+
+    }
+
+
+
+}
