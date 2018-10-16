13f36a4fd7bdb52a5eb37fa206f3bad338303510
Nicholas Falcone <nicholasfal@live.it>
2018-10-16 15:31:22 +0200

    Added Base Level Example

--- Assets/Project/Scripts/Bullet.cs
+++ Assets/Project/Scripts/Bullet.cs
@@ -3,15 +3,15 @@
 public class Bullet : MonoBehaviour
 {
     public float speed;
-    public Rigidbody m_rb;
+    public Rigidbody2D m_rb;
 
     private void Awake()
     {
-        m_rb = GetComponent<Rigidbody>();
+        m_rb = GetComponent<Rigidbody2D>();
     }
 
-    public void Init(Vector3 direction)
+    public void Init(Vector2 direction)
     {
-        m_rb.AddForce(direction * speed,ForceMode.Impulse);
+        m_rb.AddForce(direction * speed,ForceMode2D.Impulse);
     }
 }
---
+++ Assets/Project/Scripts/MovementComponent.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class MovementComponent : MonoBehaviour
+{
+    [SerializeField]
+    private float m_speed = 1;
+    [SerializeField]
+    private float m_jumpForce = 10;
+    private Collider2D[] m_colliders;
+    private Animator m_animator;
+    private Rigidbody2D m_rigidbody;
+    private SpriteRenderer m_render;
+    [SerializeField]
+    private LayerMask m_walkableLayer;
+
+    private void Awake()
+    {
+        //Get all component
+        m_animator = GetComponent<Animator>();
+        m_render = GetComponent<SpriteRenderer>();
+        m_rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    public void Jump()
+    {
+        if (IsGrounded())
+            m_rigidbody.AddForce(Vector2.up * m_jumpForce, ForceMode2D.Impulse);
+    }
+
+    public void Movement(float _direction)
+    {
+        if (_direction < 0)
+            m_render.flipX = true;
+        else
+            m_render.flipX = false;
+
+        m_rigidbody.velocity = new Vector2(_direction * m_speed, m_rigidbody.velocity.y);
+
+        if (IsGrounded())
+        {
+            float magintude = m_rigidbody.velocity.magnitude;
+            m_animator.SetFloat("Speed", magintude);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, 0.8f, m_walkableLayer);
+    }
+
+    public void PlayFootStep()
+    {
+        if(IsGrounded())
+            Debug.Log("FootStep");
+    }
+}
