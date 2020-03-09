68b6350ba481650f35ec789793b1dcc7a44843cc
Nicholas <nicholasfal@live.it>
2020-03-09 09:57:37 +0100

    bank update

---
+++ Assets/Project/Scripts/FMODSettings.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class FMODSettings : MonoBehaviour
+{
+    private const string m_masterBusName = "Bus:/";
+    private Bus m_masterBus;
+
+    void Start()
+    {
+        m_masterBus = RuntimeManager.GetBus(m_masterBusName); 
+    }
+
+    public void SetBusValue(float _nextValue)
+    {
+        m_masterBus.setVolume(_nextValue);
+    }
+}
